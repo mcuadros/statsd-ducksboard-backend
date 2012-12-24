5210fbfba917db636ba790e66fdef91f2b3d6627
Máximo Cuadros Ortiz <mcuadros@gmail.com>
2012-12-24 04:17:14 +0100

    Cache system

---
+++ src/Cache.js
@@ -0,0 +1,34 @@
+var FileSystem = require('fs');
+
+function Cache(config){
+    var self = this;
+    this.folder = config.cache || null;
+};
+
+Cache.prototype.save = function(metric, data) {
+    if ( !this.folder ) return null;
+    return FileSystem.writeFileSync(this.getFilename(metric), JSON.stringify(data));
+};
+
+Cache.prototype.read = function(metric) {
+    if ( !this.folder ) return null;
+
+    var file = this.getFilename(metric);
+    if ( !this.fileExists(file) ) return null;
+
+    var data = FileSystem.readFileSync(file);
+    return JSON.parse(data);
+};
+
+Cache.prototype.getFilename = function(metric) {
+    return this.folder + metric + '.json';
+};
+
+Cache.prototype.fileExists = function(file) {  
+    try { return FileSystem.statSync(file); }  
+    catch(error) { return false; }  
+};
+
+exports.cache = Cache;
+
+
