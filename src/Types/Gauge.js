0164adc9f41fbc421f85bfc4689a6650c529526e
Máximo Cuadros Ortiz <mcuadros@gmail.com>
2012-12-25 23:16:27 +0100

    Finished Gauge and Number, i hope

--- src/Types/Gauge.js
+++ src/Types/Gauge.js
@@ -6,8 +6,9 @@ function Gauge(name, config){
 
     this.defaultType = 'sum';
     this.metric = config.metrics.name;
-   // if ( config.from.name ) this.valid[config.from.name] = config.from;
 
+    this.from = config.from;
+    if ( !config.from.value ) this.add(config.from);
 };
 
 util.inherits(Gauge, Type);
@@ -15,9 +16,11 @@ Gauge.prototype.payload = function() {
     var value = this.value(this.metric);
     if ( !value ) return false;
 
-    var from = this.config.from.value;
-    if ( !from ) from = this.value(this.config.from.name)
-    else return {value:value/from};  
+    if ( !this.from.value ) var from = this.value(this.from.name);
+    else var from = {data: this.from.value}; 
+
+    if ( !from.data || !value.data ) return false;
+    return {value:value.data/from.data};  
 };
 
 exports.type = Gauge;
